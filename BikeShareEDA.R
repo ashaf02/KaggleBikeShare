ce9a0ff7165a91b4db3d5ed885b4021ab577eea7
Amanda Shaffer <amandanicole.shaffer@gmail.com>
2023-09-13 13:04:28 -0600

    adding 4 panel plot

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -5,8 +5,42 @@
 ##Libraries
 library(tidyverse)
 library(vroom)
+library(ggplot2)
+install.packages("DataExplorer")
 
 ##Read in the Data
-bike <- vroom("./train.csv")
+bike <- vroom("train.csv")
 
-train
+##Identify variables
+head(bike)
+ls(bike)
+
+##All data is complete and there are no missing observations. Most of the variables are continuous. 
+DataExplorer::plot_intro(bike)
+
+##Creating 4 panel plot 
+plot1 <- ggplot(bike, aes(x = atemp, y = count)) +
+  geom_point() +
+  labs(title = "Scatter Plot: atemp vs. count") +
+  theme_minimal()
+
+plot2 <- ggplot(bike, aes(x = workingday, fill = workingday, y = ..count..)) +
+  geom_bar() +
+  labs(title = "Bar Plot: Season vs. Count") +
+  theme_minimal()
+
+plot3 <- ggplot(bike, aes(x = datetime, y = count)) +
+  geom_line() +
+  labs(title = "Count over Time") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+plot4 <- ggplot(bike, aes(x = weather, y = count, fill = weather)) +
+  geom_boxplot() +
+  labs(title = "Box Plot: Weather vs. Count") +
+  facet_wrap(~weather) +
+  theme_minimal()
+
+# Combine the plots into a 2x2 grid
+library(gridExtra)
+grid.arrange(plot1, plot2, plot3, plot4, ncol = 2)
