ee8c35d9697e567037fabca20faee0990555094f
Amanda Shaffer <amandanicole.shaffer@gmail.com>
2023-09-18 13:41:14 -0600

    poisson

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,75 +1,74 @@
-
-##Installing packages
-install.packages("tidymodels")
-install.packages("tidyverse")
-
-##Libraries
+## Libraries I am going to need
 library(tidyverse)
 library(tidymodels)
 library(vroom)
-library(recipes)
-library(dplyr)
-library(tibble)
 
-##Read in the Data
-bike <- vroom("train.csv")
-test <- vroom("test.csv")
+## Read in the data
+bikeTrain <- vroom("./data/train.csv")
+bikeTest <- vroom("./data/test.csv")
 
-test
+## Remove casual and registered because we can't use them to predict
+bikeTrain <- bikeTrain %>%
+  select(-casual, - registered)
 
-test <- test %>%
-  mutate(
-    datetime = ymd_hms(datetime),  # Convert to datetime format
-    datetime = format(datetime, format = "%m/%d/%Y %H:%M")  # Format as '1/20/2011 0:00'
-  )
+## Cleaning & Feature Engineering
+bike_recipe <- recipe(count~., data=bikeTrain) %>%
+  step_mutate(weather=ifelse(weather==4, 3, weather)) %>% #Relabel weather 4 to 3
+  step_mutate(weather=factor(weather, levels=1:3, labels=c("Sunny", "Mist", "Rain"))) %>%
+  step_mutate(season=factor(season, levels=1:4, labels=c("Spring", "Summer", "Fall", "Winter"))) %>%
+  step_mutate(holiday=factor(holiday, levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_mutate(workingday=factor(workingday,levels=c(0,1), labels=c("No", "Yes"))) %>%
+  step_time(datetime, features="hour") %>%
+  step_rm(datetime)
 
-##Cleaning
-bike_cleaned <- bike %>%
-  mutate(weather = ifelse(weather == 4, 3, weather))%>%
-  select(-casual, -registered)
+prepped_recipe <- prep(bike_recipe)
+bake(prepped_recipe, new_data = bikeTrain) #Make sure recipe work on train
+bake(prepped_recipe, new_data = bikeTest) #Make sure recipe works on test
 
-bike_cleaned
-
-##Feature Engineering
-my_formula <- count ~ datetime + season + holiday + workingday + weather + temp + atemp + humidity + windspeed
-
-my_recipe <- recipe(my_formula, data = bike_cleaned) %>%
-  # Convert 'season', 'holiday', 'workingday', and 'weather' to factors
-  step_mutate(
-    season = factor(season, levels = c(1, 2, 3, 4), labels = c("spring", "summer", "fall", "winter")),
-    holiday = factor(holiday, levels = c(0, 1), labels = c("no", "yes")),
-    workingday = factor(workingday, levels = c(0, 1), labels = c("no", "yes")),
-    weather = factor(weather, levels = c(1, 2, 3), labels = c("Clear", "Mist", "Light Rain"))
-  )
-
-prepped_recipe <- prep(my_recipe) # Sets up the preprocessing using myDataSet14
-bake(prepped_recipe, new_data=test)
-
-##Linear Regression 
-my_mod <- linear_reg() %>% 
+## Define the model
+lin_model <- linear_reg() %>%
   set_engine("lm")
 
-bike_workflow <- workflow() %>% 
-  add_recipe(my_recipe) %>% 
-  add_model(my_mod) %>% 
-  fit(data = bike_cleaned)
+## Set up the whole workflow
+bike_workflow <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(lin_model) %>%
+  fit(data=bikeTrain)
 
-#Create predictions
-bike_predictions <- predict(bike_workflow, 
-                            new_data=test)
+## Look at the fitted LM model this way
+extract_fit_engine(bike_workflow) %>%
+  summary()
 
-# Change 'NA' predictions to 0
-bike_predictions <- mutate(bike_predictions, .pred = ifelse(is.na(.pred), 0, .pred))
+## Get Predictions for test set AND format for Kaggle
+test_preds <- predict(bike_workflow, new_data = bikeTest) %>%
+  bind_cols(., bikeTest) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and predictions
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 
-# Round negative predictions to 0
-bike_predictions <- mutate(bike_predictions, 
-  .pred = ifelse(.pred < 0, 0, .pred))
+## Write prediction file to CSV
+vroom_write(x=test_preds, file="./TestPreds.csv", delim=",") (edited) 
 
-#Format properly
-submission_df <- tibble(
-  datetime = test$datetime,   # Assuming 'datetime' is a column in your 'test' data frame
-  count = bike_predictions$.pred
-)
+## Trying Poisson Model
+pois_mod <- poisson_reg() %>% #Type of model
+  set_engine("glm") # GLM = generalized linear model
 
-# Write the submission CSV file using vroom::vroom_write with column names
-vroom::vroom_write(submission_df, "mytest.csv")
+bike_pois_workflow <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(pois_mod) %>%
+  fit(data = bikeTrain) # Fit the workflow
+
+bike_predictions_pois <- predict(bike_pois_workflow,
+                                 new_data=bikeTest) # Use fit to predict
+
+## Get Predictions for test set AND format for Kaggle
+test_preds_pois <- predict(bike_pois_workflow, new_data = bikeTest) %>%
+  bind_cols(., bikeTest) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and predictions
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write prediction file to CSV
+vroom_write(x=test_preds_pois, file="poisTest.csv", delim=",")
